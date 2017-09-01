83b0dbfb159b755f36b74699025c0a0f3f203eb4
CodeZaru <teamsullivank@gmail.com>
2017-09-01 06:00:15 -0400

    file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,60 +17,16 @@ var wordArray=new Array;
 var previousGuesses=new Array;
 
 var numberOfCrystalGems=4;
+var crystalGemImages=['assets/images/greenGod.png','assets/images/heartGem.png','assets/images/monkeyGem.png','assets/images/unicornGem.png'];
+//var crystalGemImages=['apple','strawberry','blueberry','peach'];
 var randomNumber;
-var userNumber;
+var userNumber=88;
 var wrongAnswerCount;
 var counter = 0;
 var wins = 0;
 var losses = 0;
 $('#win').text(wins);
 $('#loss').text(losses);
-
-
-
-////////////////////////////////////////////////////
-//EXTERNAL DATA CALL: gets json data////////////////
-////////////////////////////////////////////////////
-
-
-//selects the words from json from wordBank file 
-$.getJSON('wordBank.json', function(data) { 
-//maps sub-arrays to index values
-for(i=0;i<data.wordlist.length;i++){ 
-	wordBank[i]=new Array;
-	wordBank[i][0]=data.wordlist[i].word;
-	console.log(wordBank[i][0]=data.wordlist[i].word);
-	wordBank[i][1]=data.wordlist[i].clue;
-	console.log(wordBank[i][1]=data.wordlist[i].clue);	
-}
-})//gtjson
-
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-//FUNCTIONS: declares all the game functions////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-
-
-
-////////////////////////////////////////////////////
-//FUNCTION getWord() BEGINS/////////////////////////
-////////////////////////////////////////////////////
-//selects random from the wordBank, then sets currentWord equal to index [0] of JSON subarray (declared at top), 
-//splits it up and creates a wordArray, variable defined at the top, to be used in the handleKeyUp option.			
-//split() method is used to split a string into an array of substrings, and returns the new array.
-//If an empty string ("") is used as the separator, the string is split between each character.
-function getWord(){
-	var rnd=Math.floor(Math.random()*wordBank.length);
-	currentWord=wordBank[rnd][0];
-	currentClue=wordBank[rnd][1];
-	wordBank.splice(rnd,1); 
-	wordArray=currentWord.split("");
-	console.log(currentWord);			
-}//getword
-////////////////////////////////////////////////////
-//FUNCTION getWord() ENDS/////////////////////////
-////////////////////////////////////////////////////
 
 
 ////////////////////////////////////////////////////
@@ -93,41 +49,124 @@ function titleScreen(){
 function gameScreen(){
 
 	$('#gameContent').empty();
-	//$('#gameContent').append('<div id="pixHolder"><img id="hangman" src="assets/images/hangman.png"></div>');
-	$('#gameContent').append('<div id="randomNumberHolder"></div>');
-	$('#gameContent').append('<div id="userNumberHolder"></div>');
+	$('#gameContent').append('<div id="randomNumberHolder">Target Number:</div>');
+	$('#gameContent').append('<div id="playerNumberHolder">Player Number:</div>');
 	$('#gameContent').append('<div id="crystalGemHolder"></div>');
 	$('#gameContent').append('<div id="feedback"></div>');
-	$('#gameContent').append('<div id="pixFeedback"><img id="hangmanFeedbackGameOnV1" src="assets/images/hangmanFeedbackGameOnV1.png"></div>');
 	$('#gameContent').append('<form><input type="text" id="dummy" ></form>');
+
+
+//function crystalGemGenorator() {
+
+//this block resets all the values for a new game..
+//Each crystal should have a random hidden value between 1 - 12.
+//The random number shown at the start of the game should be between 19 - 120.
+//random non-repeating number
+
+
+
+//declare numbers varialbe as an array
+var cgnumbers = []
+
+//repeat until condition is met: 4 unique numbers between 1 and 12
+while(cgnumbers.length < 4){
+			//random number generator		
+			var uniqueRandomNumber = Math.floor(Math.random()*12+1)
+			
+			//var to check the condition of unique element of four  
+			var found = false;
+			//check the random number against the numbers in the array  
+			for (var i=0; i< cgnumbers.length; i++){
+				if (cgnumbers[i] == uniqueRandomNumber){
+					found = true; break
+				}//break out of loop if randomNumber isn't 
+//unique--found in array is true...so go to top of loop and regenerate random unber..
+}
+			  //similar to hangman--if not found in the array
+			  if(!found)cgnumbers[cgnumbers.length]=uniqueRandomNumber;
+			}//the if above is like an otherwise
+//leave the loop when have 4 unique and random numbers between 1 and 12.
+
+console.log(cgnumbers);		
+var cgImages=[];
+
+for (i = 0; i < cgnumbers.length; i++) {
+	var cgImage = $('<img>');
+	cgImage.attr('data-num', cgnumbers[i]);
+	cgImage.attr('src', crystalGemImages[i]);
+	cgImage.addClass('crystalGemImage');
+	cgImages.push(cgImage);
+	$('#crystalGemHolder').append(cgImages);
+};
+console.log(cgImages);		
+
 
 
 //the getword function, defined on line 62, and then creates the approptiate number of tiles
 //this block also initiates the WAC count and the PG array. 			
 //getCrystalGems();
 
-var numberOfTiles=numberOfCrystalGems;
-console.log(numberOfTiles);
-wrongAnswerCount=0;
-previousGuesses=[];
+//var numberOfTiles=numberOfCrystalGems;
+//console.log(numberOfTiles);
+
+
+//wrongAnswerCount=0;
+//previousGuesses=[];
 
 // Need to display wrongAnswerCount	as a score at top...
+
 
 //based onthe number of letters in the current word, this creates the appropriate number of tiles
 // in div wordHolder by appending new tile class divs (#t) It iterates through the collection,
 // concatenations div #t for each item in the loop per the i loop counter.
-for(i=0;i<numberOfTiles;i++){
+/*
+for(i=0;i<cgImages.length;i++){
+
 	$('#crystalGemHolder').append('<div class="tile" id=t'+i+'></div>');
+	$('.t'+i+'').append(cgImages[i]);
+	console.log(cgImages[i]);
+
+
+};
+*/
+//appends random target number to its div	
+//$('#randomNumberHolder').append("test");
+		counter = 0;
+		$('#playerNumberHolder').text(counter);
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//appends random target number to its div	
-$('#randomNumberHolder').append("test");
+		var targetNumber = getRandomInt(19,120);
+console.log(targetNumber);
 
-//on keyup calls the handlekeyup main function
-//$(document).on("keyup",handleKeyUp);
-//mobile screen trick for  virtual keyboard access.	
-$(document).on("click",function(){$('#dummy').focus();});
-$('#dummy').focus();
+		$('#randomNumberHolder').text(targetNumber);
+
+	$('.crystalGemImage').on('click', function(){
+		    counter = counter + parseInt($(this).data('num'));
+		   console.log('num');
+		    $('#playerNumberHolder').text(counter);
+console.log(counter);
+//Victory
+
+		    if (counter == targetNumber){
+		      $('#feedback').text('Good Job!!!!');
+		      wins ++;
+		      $('#win').text(wins);
+		      console.log(wins)
+
+		      newGame();
+//defeat		        
+		    } else if ( counter > targetNumber){
+		        $('#status').text('You are not very smart!')
+		        losses ++;
+		        $('#loss').text(losses);
+		        console.log(losses)
+
+		        newGame();
+		    }
+		});
 
 }//gamescreen
 ////////////////////////////////////////////////////
@@ -137,26 +176,19 @@ $('#dummy').focus();
 
 
 /////////////////////////////////////////////////////
-//FUNCTION crystalGemGenorator() BEGINS//////////////
+//FUNCTION playGame() BEGINS//////////////
 /////////////////////////////////////////////////////
-//this block resets all the values for a new game..
-function crystalGemGenorator() {
+function gamePlay() {
 
-//declare and initiate the input, important, also used for form entry in the gamecontent div form,
-//code converts keycode to lower case string value		
-var input=String.fromCharCode(event.keyCode).toLowerCase();
 
-//and use the push() method to add new items to the end of an array, and return the new length.				
-//{
-	previousGuesses.push(input);
 
-//if the input character = a character in the wordArray of the currentWord, then set 'found' flag to true				
-//and appends the key input letter to the div tile of the same matching index value
-if(input==wordArray[i]){found=true;$('#t'+i).append(input);}	
+	
+	}
 
-}//crystalGemGenorator
+
+
 ////////////////////////////////////////////////////
-//FUNCTION handleKeyUp() ENDS///////////////////////
+//FUNCTION playGame() ENDS///////////////////////
 ////////////////////////////////////////////////////
 
 
@@ -181,36 +213,6 @@ if(currentAnswer==currentWord){
 //FUNCTION checkAnswer() ENDS///////////////////////
 ////////////////////////////////////////////////////
 
-
-////////////////////////////////////////////////////
-//FUNCTION wrongAnswer(a) BEGINS////////////////////
-////////////////////////////////////////////////////
-//called and passed in 'input' variable/argument as is expects to be passed 'a' 		
-//function wrongAnswer(a){
-//increment the wrongAnswer count
-//wrongAnswerCount++;
-//set position equal to wrongAnswer count times -75px (basically a cumulative effect on image position)	
-//var pos=(wrongAnswerCount*-75) +"px"
-//guesses div is where we display the content of the previousGuesses Array.
-//appends a space and the 'input' that was passed for 'a'
-//$('#guesses').append("  "+a);
-//shift hangman picture position
-//$('#hangman').css("left",pos);
-//Trigger defeat message if wronganswerCount is 6
-//if(wrongAnswerCount==6){
-//	defeatMessage();}
-//}//wronganswer
-////////////////////////////////////////////////////
-//FUNCTION wrongAnswer(a) ENDS////////////////////
-////////////////////////////////////////////////////
-
-
-
-// Need to manage focus
-//NOTE that: The keyup event is sent to an element when the user releases a key on the keyboard.
-//It can be attached to any element, but the event is only sent to the element that has the focus.
-//Focusable elements can vary between browsers, but form elements can always get focus 
-//so are reasonable candidates for this event type. (from https://api.jquery.com/keyup/).		
 
 ////////////////////////////////////////////////////
 //FUNCTION victoryMessage() BEGINS//////////////////
